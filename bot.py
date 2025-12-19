f9b390c179c2eda637525611e16ccbae57556694
RootCellar <RootCellar9877@gmail.com>
2025-12-18 16:11:20 -0900

    add message section

--- bot.py
+++ bot.py
@@ -24,6 +24,44 @@ intents.typing = True
 bot = commands.Bot(command_prefix='rc!', intents=intents)
 
 #
+# Message
+#
+
+@bot.event
+async def on_message(message):
+    # Log all messages
+    username = compute_printed_user(message.author)
+
+    log(f"Message: [{compute_printed_channel_for_message(message)}] {username} {message.content}")
+
+    # Only handle messages from other users
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    # Required to use discord.py builtin command processing
+    await bot.process_commands(message)
+
+def compute_printed_channel_for_message(message):
+    channel = message.channel
+    return compute_printed_channel(channel)
+
+def compute_printed_channel(channel):
+    if isinstance(channel, discord.TextChannel):
+        return f"Channel {channel.guild.name}/#{channel.name}"
+    elif isinstance(channel, discord.Thread):
+        return f"Thread {channel.guild.name}/<{channel.name}>"
+    elif isinstance(channel, discord.DMChannel):
+        return f"DM"
+
+    return "<UNKNOWN>"
+
+def compute_printed_user(user):
+    return f"<{user.name}>"
+
+#
 # RUN
 #
 
