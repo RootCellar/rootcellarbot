634cced8d2094d6b4d8159b396a4030d8a7e9d28
RootCellar <RootCellar9877@gmail.com>
2025-12-18 16:13:44 -0900

    add log function and activity section

--- bot.py
+++ bot.py
@@ -23,6 +23,53 @@ intents.typing = True
 
 bot = commands.Bot(command_prefix='rc!', intents=intents)
 
+def log(message):
+    now = datetime.datetime.now()
+    formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+    formatted_message = strip_non_ascii(message)
+    message_to_write = f"{formatted_datetime} - {formatted_message}"
+    print(message_to_write)
+    with open('info.log', 'a') as file:
+        file.write(f"{message_to_write} \n")
+
+#
+# Activity
+#
+
+@bot.event
+async def on_presence_update(before, after):
+    handle_online_status_change(before, after)
+    handle_activity_change(before, after)
+
+def handle_online_status_change(before, after):
+    if before.status == after.status:
+        return
+
+    username = compute_printed_user(after)
+
+    if after.status == discord.Status.online:
+        log(f"Status: {username} is now online")
+    elif after.status == discord.Status.idle:
+        log(f"Status: {username} is now idle")
+    elif after.status == discord.Status.do_not_disturb:
+        log(f"Status: {username} is now in Do Not Disturb")
+    elif after.status == discord.Status.offline:
+        log(f"Status: {username} is now offline")
+
+def handle_activity_change(before, after):
+    username = compute_printed_user(after)
+    before_spotify = get_spotify_activity(before.activities)
+    after_spotify = get_spotify_activity(after.activities)
+
+    if after_spotify != before_spotify and isinstance(after_spotify, discord.Spotify):
+        log(f"Status: {username} is now listening to {after_spotify.artist} - {after_spotify.title}")
+
+def get_spotify_activity(activities):
+    for activity in activities:
+        if isinstance(activity, discord.Spotify):
+            return activity
+    return None
+
 #
 # Message
 #
