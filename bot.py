84e4c78740cafc4245494396e906ea218d0e72c4
RootCellar <RootCellar9877@gmail.com>
2026-02-21 17:29:43 -0900

    implement user_has_permission_in_server()

--- bot.py
+++ bot.py
@@ -506,6 +506,17 @@ def is_user_bot_admin_for_server(user_id: int, server_id: int):
     return False
 
 
+def user_has_permission_in_server(user_id: int, server_id: int, permission: str):
+    # If the user is an admin in the server, they automatically have permission to do *anything*
+    if is_user_bot_admin_for_server(user_id, server_id) is True:
+        return True
+
+    permission_value = get_server_user_permission_value(server_id, user_id, permission)
+    if permission_value is True:
+        return True
+    return False
+
+
 async def send_dm_to_user(user: discord.User, message: str = None, embed: discord.Embed = None, silent: bool = False):
     details_list = []
 
